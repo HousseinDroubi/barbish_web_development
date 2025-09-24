a3bf404453e3c04d3cb1fa7d4038afaf42d596ac
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-09-24 21:35:31 +0300

    js: add until data types

---
+++ js/04_data_types/scripts/index.js
@@ -0,0 +1,26 @@
+// number
+console.log(120);
+console.log(120.9);
+
+// String
+console.log("Hi there");
+
+// Boolean
+console.log(true);
+console.log(false);
+
+// undefined
+console.log(undefined);
+
+// null
+console.log(null);
+
+// object
+console.log([10, 20, 30]);
+console.log({
+  name: "Houssein",
+  age: 28,
+});
+
+// typeof
+console.log(typeof "Hi there");
