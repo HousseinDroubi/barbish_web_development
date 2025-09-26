6a7d890d7cfcc8a0fdfc219b1fdbdc1e52fb058a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-09-26 22:22:31 +0300

    js: add from conditional statements

---
+++ js/11_conditional_statements/01_if/scripts/index.js
@@ -0,0 +1,12 @@
+let number = 11;
+
+if (number >= 0) {
+  console.log("number is positive");
+}
+
+if (number % 2 != 0) {
+  console.log("number is odd");
+}
+
+// 2 4 6 8 10 12 14 16 -> even
+// 1 3 5 7 9 11-> odd
---
+++ js/11_conditional_statements/02_if_else/scripts/index.js
@@ -0,0 +1,13 @@
+let number = -15;
+
+if (number >= 0) {
+  console.log("number is positive");
+} else {
+  console.log("number is negative");
+}
+
+if (number % 2 != 0) {
+  console.log("number is odd");
+} else {
+  console.log("number is even");
+}
